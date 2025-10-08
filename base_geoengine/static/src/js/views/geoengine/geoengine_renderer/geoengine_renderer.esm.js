74439ebd062bf38e0cca22a657af18684152f184
anusrinps96 <aprakhasam@nps100.com>
2025-10-08 08:41:00 +0200

    [IMP] base_geoengine: Fix Vector Layer related Error
        Notifying user to define selected attribute_field_id of
    supported type from vector layer as part of geoengine view
    xml defintion which avoids observed JS error.

--- base_geoengine/static/src/js/views/geoengine/geoengine_renderer/geoengine_renderer.esm.js
+++ base_geoengine/static/src/js/views/geoengine/geoengine_renderer/geoengine_renderer.esm.js
@@ -31,7 +31,9 @@ import {
 } from "@web/model/relational_model/utils";
 import {evaluateExpr} from "@web/core/py_js/py";
 import {loadBundle, loadJS} from "@web/core/assets";
+import {_t} from "@web/core/l10n/translation";
 import {getTemplate} from "@web/core/templates";
+import {sprintf} from "@web/core/utils/strings";
 import {parseXML} from "@web/core/utils/xml";
 import {rasterLayersStore} from "../../../raster_layers_store.esm";
 import {registry} from "@web/core/registry";
@@ -67,6 +69,7 @@ export class GeoengineRenderer extends Component {
         this.orm = useService("orm");
         this.view = useService("view");
         this.fields = useService("field");
+        this.notification = useService("notification");
 
         // For related model we need to load all the service needed by RelationalModel
         this.services = {};
@@ -783,8 +786,12 @@ export class GeoengineRenderer extends Component {
         const fields_to_read = this.getFieldsToRead(vector);
         const data = await this.getModelData(vector, fields_to_read);
         this.useRelatedModel(vector, layer, data);
-        const styleInfo = this.styleVectorLayer(vector, data);
-        this.initLegend(styleInfo, vector);
+        if (this.checkAttributeFieldUsage(vector, data)) {
+            const styleInfo = this.styleVectorLayer(vector, data);
+            if (styleInfo) {
+                this.initLegend(styleInfo, vector);
+            }
+        }
     }
 
     async renderVectorLayers() {
@@ -878,9 +885,13 @@ export class GeoengineRenderer extends Component {
     }
 
     styleVectorLayerAndLegend(cfg, data, lv) {
-        const styleInfo = this.styleVectorLayer(cfg, data);
-        this.initLegend(styleInfo, cfg);
-        lv.setStyle(styleInfo.style);
+        if (this.checkAttributeFieldUsage(cfg, data)) {
+            const styleInfo = this.styleVectorLayer(cfg, data);
+            if (styleInfo) {
+                this.initLegend(styleInfo, cfg);
+                lv.setStyle(styleInfo.style);
+            }
+        }
     }
 
     initLegend(styleInfo, cfg) {
@@ -1316,6 +1327,27 @@ export class GeoengineRenderer extends Component {
     extractLayerValues(cfg, data) {
         var indicator = cfg.attribute_field_id[1];
         return data.map((item) => item._values[indicator]);
+    }
+
+    /**
+     * Check vector Layer Attribute Field is defined
+     * by view to display proper legends
+     */
+    checkAttributeFieldUsage(cfg, data) {
+        const indicator_values = this.extractLayerValues(cfg, data);
+        if (indicator_values.some((item) => typeof item === "undefined")) {
+            this.notification.add(
+                sprintf(
+                    _t('Customize view to use Attribute Field: "%s"'),
+                    cfg.attribute_field_id[1]
+                ),
+                {
+                    type: "warning",
+                }
+            );
+            return false;
+        }
+        return true;
     }
 }
 
