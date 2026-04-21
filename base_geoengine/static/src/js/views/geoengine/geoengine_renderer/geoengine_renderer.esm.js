4bb6583b3b317dd7c81510022583007f98774f9b
anusrinps96 <aprakhasam@nps100.com>
2026-04-21 15:20:40 +0200

    [16.0][IMP] base_geoengine: Avoid error seen clicking on RecordPanel
       When a record in record panel does not contain latitude and
    longitude details, it fails with error "feature is null". Avoiding
    this error and notifying the user to update required details to
    view the record.

--- base_geoengine/static/src/js/views/geoengine/geoengine_renderer/geoengine_renderer.esm.js
+++ base_geoengine/static/src/js/views/geoengine/geoengine_renderer/geoengine_renderer.esm.js
@@ -573,22 +573,35 @@ export class GeoengineRenderer extends Component {
     onDisplayPopupRecord(record) {
         const popup = this.getPopup();
         const feature = this.vectorSource.getFeatureById(record.resId);
-        if (feature !== undefined) {
-            this.mountGeoengineRecord({
-                popup,
-                archInfo: this.props.archInfo,
-                templateDocs: this.props.archInfo.templateDocs,
-                record,
+        if (feature === null) {
+            this.notification.add(
+                sprintf(
+                    this.env._t(
+                        'Please update latitude and longitude details for: "%s"'
+                    ),
+                    record.data.display_name
+                ),
+                {
+                    type: "warning",
+                }
+            );
+            this.clickToHidePopup();
+            return false;
+        }
+        this.mountGeoengineRecord({
+            popup,
+            archInfo: this.props.archInfo,
+            templateDocs: this.props.archInfo.templateDocs,
+            record,
+        });
+        var coord = ol.extent.getCenter(feature.getGeometry().getExtent());
+        this.overlay.setPosition(coord);
+        var map_view = this.map.getView();
+        if (map_view) {
+            map_view.animate({
+                center: feature.getGeometry().getFirstCoordinate(),
+                duration: 500,
             });
-            var coord = ol.extent.getCenter(feature.getGeometry().getExtent());
-            this.overlay.setPosition(coord);
-            var map_view = this.map.getView();
-            if (map_view) {
-                map_view.animate({
-                    center: feature.getGeometry().getFirstCoordinate(),
-                    duration: 500,
-                });
-            }
         }
     }
 
